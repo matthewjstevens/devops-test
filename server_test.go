12b00759b2e92d8c99ef4479c0dccb738bad98e6
Jake Loxton-Dally <jake.loxton-dally@mclaren.com>
2024-09-18 14:13:36 +0200

    README

--- server_test.go
+++ server_test.go
@@ -3,7 +3,6 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
-	"os"
 	"testing"
 )
 
@@ -11,11 +10,11 @@ func TestGETHelloWorld(t *testing.T) {
 	request, _ := http.NewRequest(http.MethodGet, "/hello", nil)
 	response := httptest.NewRecorder()
 	message := "Hello World"
-	os.Setenv("MESSAGE", message)
+	t.Setenv("MESSAGE", message)
 
 	HelloWorldServer(response, request)
 
-	t.Run("returns hello world", func(t *testing.T) {
+	t.Run("returns "+message, func(t *testing.T) {
 		got := response.Body.String()
 		want := message
 
