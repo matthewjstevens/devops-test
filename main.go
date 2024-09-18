27794ab986d36006e48831dfabb23381ec39dded
Jake Loxton-Dally <jake.loxton-dally@mclaren.com>
2024-09-18 15:29:45 +0200

    Change logging to use logrus

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"log"
-	"log/slog"
 	"net/http"
+
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	slog.Info("starting up")
+	log.SetReportCaller(true)
+	log.Info("starting up")
 	http.HandleFunc("/hello", HelloWorldServer)
 	log.Fatal(http.ListenAndServe(":5000", nil))
 }
--- server.go
+++ server.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
-	"log/slog"
 	"net/http"
 	"os"
+
+	log "github.com/sirupsen/logrus"
 )
 
 // HelloWorldServer returns a message set by env var for any request
 func HelloWorldServer(w http.ResponseWriter, r *http.Request) {
-	slog.Info("handler hit")
+	log.Info("handler hit")
 	fmt.Fprint(w, os.Getenv("MESSAGE"))
 }
